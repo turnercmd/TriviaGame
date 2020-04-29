0ffb605895432dcaaf5fe5982ee83219e5e1c2c6
Jeff Turner <jeffgill57@gmail.com>
2020-04-29 15:45:07 -0500

    Added questions to JS

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -19,100 +19,103 @@ function generateQuiz(questions, quizContainer, resultsContainer, submitBtn){
 var myQuestions =[
 {
     question: "What is the best football team of all time?",
-answers: {
-    a:"New England Patriots",
-    b:"Pitsburgh Steelers",
-    c:"San Francisco 49ers",
-    d:"Miami Dolphins",
-}
+    answers: {
+        a:"New England Patriots",
+        b:"Pitsburgh Steelers",
+        c:"San Francisco 49ers",
+        d:"Miami Dolphins",
+    },
+    correctAnswer: "c"
+    },
 
 {
-    question: "What is the best football team of all time?",
+    question: "What number does Tom Brady wear on his jersey?",
     answers: {
-        a:
-        b:
-        c:
-        d:
+        a:"7",
+        b:"12",
+        c:"8",
+        d:"342",
     },
-    correctAnswer:
-}
+    correctAnswer:"b"
+},
 {
-    question: "What is the best football team of all time?",
+    question: "Who is the greatest Quarterback of all time?",
     answers: {
-        a:
-        b:
-        c:
-        d:
+        a:"Tom Brady",
+        b:"Fran Tarkington",
+        c:"Brett Favre",
+        d:"Joe Montana",
     },
-    correctAnswer:
-}
+    correctAnswer:"d"
+},
 {
-    question: "What is the best football team of all time?",
+    question: "What NFL team plays in Las Vegas, NV?",
     answers: {
-        a:
-        b:
-        c:
-        d:
+        a:"Falcons",
+        b:"Raiders",
+        c:"Texans",
+        d:"Panthers",
     },
-    correctAnswer:
-}
+    correctAnswer:"b"
+},
 {
-    question: "What is the best football team of all time?",
+    question: "Who is the Commishioner of the NFL?",
     answers: {
-        a:
-        b:
-        c:
-        d:
+        a:"Roger Goodell",
+        b:"Adam Silver",
+        c:"Adam Schefter",
+        d:"Bernie Sanders",
     },
-    correctAnswer:
-}
+    correctAnswer:"a"
+},
 {
-    question: "What is the best football team of all time?",
+    question: "Who performed at the 2020 Superbowl?",
     answers: {
-        a:
-        b:
-        c:
-        d:
+        a:"Maroon 5",
+        b:"The Rolling Stones",
+        c:"Kesha",
+        d:"Jennifer Lopez",
     },
-    correctAnswer:
-}
+    correctAnswer:"d"
+},
 {
-    question: "What is the best football team of all time?",
+    question: "How many teams are in the NFL?",
     answers: {
-        a:
-        b:
-        c:
-        d:
+        a:"50",
+        b:"28",
+        c:"32",
+        d:"18",
     },
-    correctAnswer:
-}
+    correctAnswer:"c"
+},
 {
-    question: "What is the best football team of all time?",
+    question: "What number does Tom Brady wear on his jersey?",
     answers: {
-        a:
-        b:
-        c:
-        d:
+        a:"7",
+        b:"12",
+        c:"8",
+        d:"342",
     },
-    correctAnswer:
-}
+    correctAnswer:"b"
+},
 {
-    question: "What is the best football team of all time?",
+    question: "Who is the Qaurterback for the Arizon Cardinals",
     answers: {
-        a:
-        b:
-        c:
-        d:
+        a:"Kyler Murray",
+        b:"Derek Carr",
+        c:"Cam Newton",
+        d:"Matt Stafford",
     },
-    correctAnswer:
-}
+    correctAnswer:"a"
+},
 {
-    question: "What is the best football team of all time?",
+    question: "Who is the Head Coach for Las Vegas Raiders",
     answers: {
-        a:
-        b:
-        c:
-        d:
+        a:"Andy Reid",
+        b:"Mike Tomlin",
+        c:"Johnny Tsunami",
+        d:"John Gruden",
     },
-    correctAnswer:
-}
+    correctAnswer:"d"
+},
+];
