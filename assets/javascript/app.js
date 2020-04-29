e1c41d96c2f0a3d241cddb78694f54ea39eac63e
Jeff Turner <jeffgill57@gmail.com>
2020-04-29 15:23:36 -0500

    Removed initial HTML layout and updated JS Questions Layout

---
+++ assets/javascript/app.js
@@ -0,0 +1,118 @@
+function generateQuiz(questions, quizContainer, resultsContainer, submitBtn){
+    
+    function displayQuestions(questions, quizContainer){
+
+    }
+
+    function displayResults(questions, quizContainer, resultsContainer){
+
+    }
+
+    displayQuestions(questions, quizContainer);
+
+    submitBtn.onclick=function(){
+
+        showResults(questions, quizContainer, resultsContainer);
+    }
+}
+
+var myQuestions =[
+{
+    question: "What is the best football team of all time?",
+answers: {
+    a:"New England Patriots",
+    b:"Pitsburgh Steelers",
+    c:"San Francisco 49ers",
+    d:"Miami Dolphins",
+}
+
+{
+    question: "What is the best football team of all time?",
+    answers: {
+        a:
+        b:
+        c:
+        d:
+    },
+    correctAnswer:
+}
+{
+    question: "What is the best football team of all time?",
+    answers: {
+        a:
+        b:
+        c:
+        d:
+    },
+    correctAnswer:
+}
+{
+    question: "What is the best football team of all time?",
+    answers: {
+        a:
+        b:
+        c:
+        d:
+    },
+    correctAnswer:
+}
+{
+    question: "What is the best football team of all time?",
+    answers: {
+        a:
+        b:
+        c:
+        d:
+    },
+    correctAnswer:
+}
+{
+    question: "What is the best football team of all time?",
+    answers: {
+        a:
+        b:
+        c:
+        d:
+    },
+    correctAnswer:
+}
+{
+    question: "What is the best football team of all time?",
+    answers: {
+        a:
+        b:
+        c:
+        d:
+    },
+    correctAnswer:
+}
+{
+    question: "What is the best football team of all time?",
+    answers: {
+        a:
+        b:
+        c:
+        d:
+    },
+    correctAnswer:
+}
+{
+    question: "What is the best football team of all time?",
+    answers: {
+        a:
+        b:
+        c:
+        d:
+    },
+    correctAnswer:
+}
+{
+    question: "What is the best football team of all time?",
+    answers: {
+        a:
+        b:
+        c:
+        d:
+    },
+    correctAnswer:
+}
