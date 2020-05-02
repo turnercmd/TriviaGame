eb58cbfbb1063df499b69bb04a434ba69f08c573
Jeff Turner <jeffgill57@gmail.com>
2020-05-02 03:34:27 -0500

    CSS errors, JS complications, timer in progress - needs (hide submit on load, unhide onclick, bin for responses not captured by submit button, window timeout, possible answer repo for recall at time expiration)

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,121 +1,191 @@
-function generateQuiz(questions, quizContainer, resultsContainer, submitBtn){
+
+    function startTimer() {
+        var seconds = 0;
+	timer = setInterval(function() {
+	    seconds ++;
+	    document.getElementById("seconds").innerText = seconds % 60;
+            document.getElementById("minutes").innerText = parseInt(seconds / 60);
+        }, 1000);
+
+
+var myQuestions =[
+    {
+        question: "Who is the best football team of all time?",
+        answers: {
+            a:"New England Patriots",
+            b:"Pitsburgh Steelers",
+            c:"San Francisco 49ers"
+        },
+        correctAnswer: "c"
+        },
+
+    {
+        question: "What number does Tom Brady wear on his jersey?",
+        answers: {
+            a:"7",
+            b:"12",
+            c:"8"
+        },
+        correctAnswer:"b"
+    },
+    {
+        question: "Who is the greatest Quarterback of all time?",
+        answers: {
+            a:"Tom Brady",
+            b:"Fran Tarkington",
+            c:"Joe Montana"
+        },
+        correctAnswer:"c"
+    },
+    {
+        question: "What NFL team plays in Las Vegas, NV?",
+        answers: {
+            a:"Falcons",
+            b:"Raiders",
+            c:"Texans"
+        },
+        correctAnswer:"b"
+    },
+    {
+        question: "Who is the Commishioner of the NFL?",
+        answers: {
+            a:"Roger Goodell",
+            b:"Adam Silver",
+            c:"Bernie Sanders"
+        },
+        correctAnswer:"a"
+    },
+    {
+        question: "Who performed at the 2020 Superbowl?",
+        answers: {
+            a:"Maroon 5",
+            b:"The Rolling Stones",
+            c:"Jennifer Lopez"
+        },
+        correctAnswer:"c"
+    },
+    {
+        question: "How many teams are in the NFL?",
+        answers: {
+            a:"50",
+            b:"28",
+            c:"32"
+        },
+        correctAnswer:"c"
+    },
+    {
+        question: "What number does Tom Brady wear on his jersey?",
+        answers: {
+            a:"7",
+            b:"12",
+            c:"8"
+        },
+        correctAnswer:"b"
+    },
+    {
+        question: "Who is the Qaurterback for the Arizon Cardinals",
+        answers: {
+            a:"Kyler Murray",
+            b:"Derek Carr",
+            c:"Cam Newton"
+        },
+        correctAnswer:"a"
+    },
+    {
+        question: "Who is the Head Coach for Las Vegas Raiders",
+        answers: {
+            a:"Andy Reid",
+            b:"John Gruden",
+            c:"Johnny Tsunami"
+        },
+        correctAnswer:"b"
+    }
+];
+
+var quizContainer = document.getElementById('quiz');
+var resultsContainer = document.getElementById('results');
+var submitButton = document.getElementById('submit');
+
+generateQuiz(myQuestions, quizContainer, resultsContainer, submitButton);
+
+function generateQuiz(questions, quizContainer, resultsContainer, submitButton){
+
+    function showQuestions(questions, quizContainer){
     
-    function displayQuestions(questions, quizContainer){
+        var output = [];
+        var answers;
 
+  
+        for(var i=0; i<questions.length; i++){
+            
+     
+            answers = [];
+
+   
+            for(letter in questions[i].answers){
+
+                answers.push(
+                    '<label>'
+                        + '<input type="radio" name="question'+i+'" value="'+letter+'">'
+                        + letter + ': '
+                        + questions[i].answers[letter]
+                    + '</label>'
+                );
+            }
+
+           
+            output.push(
+                '<div class="question">' + questions[i].question + '</div>'
+                + '<div class="answers">' + answers.join('') + '</div>'
+            );
+        }
+
+        quizContainer.innerHTML = output.join('');
     }
 
-    function displayResults(questions, quizContainer, resultsContainer){
 
+    function showResults(questions, quizContainer, resultsContainer){
+        
+      
+        var answerContainers = quizContainer.querySelectorAll('.answers');
+        
+  
+        var userAnswer = '';
+        var numCorrect = 0;
+        
+   
+        for(var i=0; i<questions.length; i++){
+
+     
+            userAnswer = (answerContainers[i].querySelector('input[name=question'+i+']:checked')||{}).value;
+            
+          
+            if(userAnswer===questions[i].correctAnswer){
+             
+                numCorrect++;
+                
+             
+                answerContainers[i].style.color = 'lightgreen';
+            }
+            
+            else
+            {
+                
+                answerContainers[i].style.color = 'red';
+            }
+        }
+
+      
+        resultsContainer.innerHTML = numCorrect + ' out of ' + questions.length;
     }
 
-    displayQuestions(questions, quizContainer);
+   
+    showQuestions(questions, quizContainer);
+    
 
-    submitBtn.onclick=function(){
-
+    submitButton.onclick = function(){
         showResults(questions, quizContainer, resultsContainer);
     }
 }
 
-var myQuestions =[
-{
-    question: "What is the best football team of all time?",
-    answers: {
-        a:"New England Patriots",
-        b:"Pitsburgh Steelers",
-        c:"San Francisco 49ers",
-        d:"Miami Dolphins",
-    },
-    correctAnswer: "c"
-    },
-
-{
-    question: "What number does Tom Brady wear on his jersey?",
-    answers: {
-        a:"7",
-        b:"12",
-        c:"8",
-        d:"342",
-    },
-    correctAnswer:"b"
-},
-{
-    question: "Who is the greatest Quarterback of all time?",
-    answers: {
-        a:"Tom Brady",
-        b:"Fran Tarkington",
-        c:"Brett Favre",
-        d:"Joe Montana",
-    },
-    correctAnswer:"d"
-},
-{
-    question: "What NFL team plays in Las Vegas, NV?",
-    answers: {
-        a:"Falcons",
-        b:"Raiders",
-        c:"Texans",
-        d:"Panthers",
-    },
-    correctAnswer:"b"
-},
-{
-    question: "Who is the Commishioner of the NFL?",
-    answers: {
-        a:"Roger Goodell",
-        b:"Adam Silver",
-        c:"Adam Schefter",
-        d:"Bernie Sanders",
-    },
-    correctAnswer:"a"
-},
-{
-    question: "Who performed at the 2020 Superbowl?",
-    answers: {
-        a:"Maroon 5",
-        b:"The Rolling Stones",
-        c:"Kesha",
-        d:"Jennifer Lopez",
-    },
-    correctAnswer:"d"
-},
-{
-    question: "How many teams are in the NFL?",
-    answers: {
-        a:"50",
-        b:"28",
-        c:"32",
-        d:"18",
-    },
-    correctAnswer:"c"
-},
-{
-    question: "What number does Tom Brady wear on his jersey?",
-    answers: {
-        a:"7",
-        b:"12",
-        c:"8",
-        d:"342",
-    },
-    correctAnswer:"b"
-},
-{
-    question: "Who is the Qaurterback for the Arizon Cardinals",
-    answers: {
-        a:"Kyler Murray",
-        b:"Derek Carr",
-        c:"Cam Newton",
-        d:"Matt Stafford",
-    },
-    correctAnswer:"a"
-},
-{
-    question: "Who is the Head Coach for Las Vegas Raiders",
-    answers: {
-        a:"Andy Reid",
-        b:"Mike Tomlin",
-        c:"Johnny Tsunami",
-        d:"John Gruden",
-    },
-    correctAnswer:"d"
-},
-];
+}
